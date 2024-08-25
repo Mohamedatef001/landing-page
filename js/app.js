bdb037e49a647f1271e5aa2441a1ebae4774b5dd
Mohamed_Atef <atefm4865@gmail.com>
2024-08-25 16:43:01 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,45 +1,8 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
 const sections = document.querySelectorAll('section');
 const ulMenu = document.querySelector('#navbar__list');
 const fragment = document.createDocumentFragment();
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
 
 // build the nav
 function navMenu() {
